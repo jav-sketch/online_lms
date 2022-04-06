467bc808e4f1b77131c7b4a07feeae0b26667b88
Javon <javonbrowne@gmail.com>
2022-04-06 14:02:44 -0400

    Added for model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from django.utils import timezone
+from datetime import datetime, timedelta
 
 # Create your models here.
+
+#Course 
+class Course(models.Model):
+    title = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=200, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
